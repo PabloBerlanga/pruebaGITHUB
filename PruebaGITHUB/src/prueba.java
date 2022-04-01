b3ef21b89ea5d4657053fd89d293543fa90ff85a
Pablo <Pablo@192.168.240.250>
2022-04-01 18:05:53 +0200

    Primer Commit

---
+++ PruebaGITHUB/src/prueba.java
@@ -0,0 +1,12 @@
+
+public class prueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("En este proyecto trabajamos con el repositorio GITHUB");
+		
+		
+	}
+
+}

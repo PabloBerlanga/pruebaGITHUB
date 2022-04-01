8bb7d83719a6081c454b7597c6102783c4637d2c
Pablo <Pablo@192.168.240.250>
2022-04-01 18:25:02 +0200

    2 Mejora de codigo

--- pruebaGIT/src/pruebaGIT/pruebaas.java
+++ pruebaGIT/src/pruebaGIT/pruebaas.java
@@ -5,7 +5,7 @@ public class pruebaas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("En este proyecto trabajamos con el repositorio GITHUB mejorado");
+		System.out.println("En este proyecto trabajamos con el repositorio GITHUB mejorado 2");
 		
 
 	}
